8408c762a0549d51199b81417c42a9a47009562f
agpelliciari <apelliciari@fi.uba.ar>
2025-08-29 20:20:47 -0300

    feat: implementaciones y modificaciones en client

--- client/common/client.go
+++ client/common/client.go
@@ -2,8 +2,11 @@ package common
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"net"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/op/go-logging"
@@ -50,11 +53,36 @@ func (c *Client) createClientSocket() error {
 	return nil
 }
 
+// Close the actual connection with log messages
+func (c *Client) closeConnection() {
+    log.Infof("action: close_connection | result: in_progress | client_id: %v", c.config.ID)
+    c.conn.Close()
+    c.conn = nil
+    log.Infof("action: close_connection | result: success | client_id: %v", c.config.ID)
+}
+
 // StartClientLoop Send messages to the client until some time threshold is met
 func (c *Client) StartClientLoop() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM)
+    defer stop()
+
 	// There is an autoincremental msgID to identify every message sent
 	// Messages if the message amount threshold has not been surpassed
 	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
+
+		select {
+        case <-ctx.Done():
+			// Graceful shutdown
+            log.Infof("action: graceful_shutdown | result: in_progress | client_id: %v | message: SIGTERM received", c.config.ID)
+            if c.conn != nil {
+                c.closeConnection()
+            }
+            log.Infof("action: graceful_shutdown | result: success | client_id: %v", c.config.ID)
+            return
+        default:
+            // No signal, continue normal operation
+        }
+
 		// Create the connection the server in every loop iteration. Send an
 		c.createClientSocket()
 
@@ -66,7 +94,7 @@ func (c *Client) StartClientLoop() {
 			msgID,
 		)
 		msg, err := bufio.NewReader(c.conn).ReadString('\n')
-		c.conn.Close()
+		c.closeConnection()
 
 		if err != nil {
 			log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
@@ -82,8 +110,16 @@ func (c *Client) StartClientLoop() {
 		)
 
 		// Wait a time between sending one message and the next one
-		time.Sleep(c.config.LoopPeriod)
-
+        select {
+        case <-ctx.Done():
+            // Graceful shutdown
+            log.Infof("action: graceful_shutdown | result: in_progress | client_id: %v | message: SIGTERM received", c.config.ID)
+            log.Infof("action: graceful_shutdown | result: success | client_id: %v", c.config.ID)
+            return
+        case <-time.After(c.config.LoopPeriod):
+            // Continue with the next message
+        }
 	}
+
 	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
 }
